7d1e7cfa1edd3780a775526cfec142c5ce126f56
empucrs <edson.moreno@pucrs.br>
2024-08-29 13:13:09 -0300

    Aula introdutoria sobre herança

---
+++ Aula_0828/exemplo_produtos/ProdutoPerecivel.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+
+public class ProdutoPerecivel extends Produto {
+    private LocalDate dataValidade;
+
+    public ProdutoPerecivel(String nome, double preco, LocalDate dataValidade) {
+        super(nome, preco);
+        //this.nome=nome;
+        //this.preco=preco;
+        this.dataValidade = dataValidade;
+    }
+
+    public LocalDate getDataValidade() {
+        return dataValidade;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "[validade=" + dataValidade;
+    }
+}
